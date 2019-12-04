73fe424276e2f5f79209541c08601a651a07af77
naveenkanakanti <naveenkanakanti@gmail.com>
2019-12-04 14:31:55 +0530

    Peripheral and Central Activity

---
+++ app/src/main/java/com/example/bluelight/CentralActivity.java
@@ -0,0 +1,32 @@
+package com.example.bluelight;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class CentralActivity extends AppCompatActivity {
+    private Button bt_scan;
+
+
+    class scanOnClickListner implements View.OnClickListener{
+        @Override
+        public void onClick(View V){
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_central);
+        bt_scan = findViewById(R.id.scan);
+    }
+
+}
---
+++ app/src/main/java/com/example/bluelight/PeripheralActivity.java
@@ -0,0 +1,30 @@
+package com.example.bluelight;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class PeripheralActivity extends AppCompatActivity {
+    private Button startAdvertisement;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peripheral);
+         startAdvertisement = findViewById(R.id.startAdvertisement);
+    }
+
+    class startadvOnClickListner implements View.OnClickListener{
+        @Override
+        public void onClick(View V){
+
+        }
+    }
+
+}
