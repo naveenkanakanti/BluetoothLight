598671ea590534c5ec558da808d2858e4d05f92b
Sachin <Sachin1998.agrl@gmail.com>
2019-12-05 12:24:10 +0530

    Fixes

--- app/src/main/java/com/example/bluelight/CentralActivity.java
+++ app/src/main/java/com/example/bluelight/CentralActivity.java
@@ -1,5 +1,11 @@
 package com.example.bluelight;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,11 +14,20 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CentralActivity extends AppCompatActivity {
+    private String TAG = "CentralActivity";
     private Button bt_scan;
+    private LinearLayout ll_DeviceList;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mBluetoothLeScanner;
 
 
 
@@ -23,12 +38,33 @@ public class CentralActivity extends AppCompatActivity {
         setContentView(R.layout.activity_central);
         bt_scan = findViewById(R.id.scan);
         bt_scan.setOnClickListener(new scanOnClickListner());
+        ll_DeviceList = findViewById(R.id.deviceList);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
     }
     class scanOnClickListner implements View.OnClickListener{
         @Override
         public void onClick(View V){
+            List<ScanFilter> bleScanFilter = new ArrayList<>();
+            mBluetoothLeScanner.startScan(bleScanFilter , new ScanSettings.Builder().
+                    setScanMode(ScanSettings.SCAN_MODE_BALANCED).build() ,mScanCallback);
 
         }
+    }
+
+    private ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+
+            Log.d(TAG,"scan result: " + result.getDevice().getAddress() + " rssi "
+                    + result.getRssi() + " name " + result.getDevice().getName());
+
+        }
+    };
+
+    private void updatelist(){
+
     }
 
 }
